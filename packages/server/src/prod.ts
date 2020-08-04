0217cfaf16830124083e39148304f6485897042e
nnn3d <nnn3d@mail.ru>
2020-08-04 23:15:03 +0300

    fix: remove env from prod

--- packages/server/src/prod.ts
+++ packages/server/src/prod.ts
@@ -17,15 +17,9 @@ import path from 'path'
     response.sendFile(path.resolve(staticDir, 'index.html'))
   })
 
-  await app.listen(
-    parseInt(process.env.PORT as string) || 80,
-    process.env.HOST || '127.0.0.1',
-  )
+  await app.listen(80, '127.0.0.1')
 
   // logux server
-  const server = await createServer({
-    port: parseInt(process.env.LOGUX_PORT as string),
-    host: process.env.HOST || '127.0.0.1',
-  })
+  const server = await createServer()
   await server.listen()
 })()
