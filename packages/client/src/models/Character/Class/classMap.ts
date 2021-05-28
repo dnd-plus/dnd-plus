04d0158c141ff6c9464b1b0e453f23b98f0a28a9
nnn3d <nnn3d@mail.ru>
2021-05-29 01:12:33 +0300

    Revert "fix: add tsconfig-paths"
    This reverts commit 05dad1d8

--- packages/client/src/models/Character/Class/classMap.ts
+++
@@ -1,4 +0,0 @@
-import { CharacterClassName } from 'common/types/base/character/CharacterClassName'
-import { CharacterClass } from 'models/Character/Class/Class'
-
-export const classMap: Record<CharacterClassName, CharacterClass> = {}
